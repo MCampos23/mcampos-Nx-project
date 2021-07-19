1f3d6cf682961fe6616fdcaf226c5ed524e5f42d
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-07-19 11:48:56 +0200

    login form token and errors

--- libs/users/src/lib/pages/login/login.component.ts
+++ libs/users/src/lib/pages/login/login.component.ts
@@ -1,35 +1,52 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'users-login',
-  templateUrl: './login.component.html',
-  styles: [
-  ]
+    selector: 'users-login',
+    templateUrl: './login.component.html',
+    styles: []
 })
 export class LoginComponent implements OnInit {
+    loginFormGroup!: FormGroup;
+    isSubmitted = false;
+    authError = false;
+    authMessage = 'El email o la contraseña son incorrectos';
 
-  loginFormGroup!: FormGroup;
-  isSubmitted = false;
+    constructor(private formBuilder: FormBuilder, private authService: AuthService) {}
 
-  constructor(private formBuilder: FormBuilder) { }
+    ngOnInit(): void {
+        this._initLoginForm();
+    }
 
-  ngOnInit(): void {
-    this._initLoginForm();
-  }
+    private _initLoginForm() {
+        this.loginFormGroup = this.formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required]]
+        });
+    }
 
-  private _initLoginForm(){
-    this.loginFormGroup = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
-    })
-  }
+    get loginForm() {
+        return this.loginFormGroup.controls;
+    }
 
-  get loginForm(){
-    return this.loginFormGroup.controls;
-  }
+    onSubmit() {
+        this.isSubmitted = true;
 
-  onSubmit(){
-    this.isSubmitted = true;
-  }
+        if (this.loginFormGroup.invalid) return;
+
+        this.authService.login(this.loginForm.email.value, this.loginForm.password.value).subscribe(
+            () => { 
+              this.authError = false;
+            },
+            (error: HttpErrorResponse) => {
+                this.authError = true;
+                if (error.status !== 400) {
+                    this.authMessage = 'Error en el servidor, intentalo de nuevo más tarde';
+                }
+            }
+        );
+    }
 }
