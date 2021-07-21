721368b47f748f77fb8b11dddf034193f53befc5
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-07-21 13:22:36 +0200

    ngeshop home page view and header

--- apps/ngeshop/src/app/app.module.ts
+++ apps/ngeshop/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { UiModule } from '@mcampos/ui'
 import {AccordionModule} from 'primeng/accordion';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NavComponent } from './shared/nav/nav.component';
 
 const routes:Routes = [
  { path: '', component: HomePageComponent },
@@ -17,7 +18,7 @@ const routes:Routes = [
 ]
 
 @NgModule({
-    declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
+    declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent,NavComponent ],
     imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes, { initialNavigation: 'enabled' }), UiModule, AccordionModule],
     providers: [],
     bootstrap: [AppComponent]
