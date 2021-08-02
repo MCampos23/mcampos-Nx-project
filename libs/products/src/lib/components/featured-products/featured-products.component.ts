5452a45c741a2a59bf3fdd778c7886055de0ae61
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-08-02 12:54:57 +0200

    featured products banner

--- libs/products/src/lib/components/featured-products/featured-products.component.ts
+++ libs/products/src/lib/components/featured-products/featured-products.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Product } from '../../models/product';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'products-featured-products',
@@ -6,11 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class FeaturedProductsComponent implements OnInit {
+export class FeaturedProductsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  featuredProducts: Product[] = []
+  endSubs$ : Subject<any>= new Subject();
+  
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this._getFeaturedProducts()
+  }
+  ngOnDestroy(): void {
+    this.endSubs$.next();
+    this.endSubs$.complete();
+  }
+
+  private _getFeaturedProducts() {
+    this.productsService.getFeaturedProducts(4)
+                        .pipe(takeUntil(this.endSubs$))
+                        .subscribe(products => {
+          
+      this.featuredProducts = Object.values(products);
+      console.log(this.featuredProducts[0])
+    
+    })
   }
 
 }
