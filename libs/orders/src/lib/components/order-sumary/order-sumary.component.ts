c32a92a43a656353c69dc19fb084ee422f1cd194
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-08-04 21:15:10 +0200

    order summary calculation

--- libs/orders/src/lib/components/order-sumary/order-sumary.component.ts
+++ libs/orders/src/lib/components/order-sumary/order-sumary.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil, take } from 'rxjs/operators';
+import { CartService } from '../../services/cart.service';
+import { OrdersService } from '../../services/ordersService';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'orders-order-sumary',
@@ -6,11 +12,38 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrderSumaryComponent implements OnInit {
+export class OrderSumaryComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  endSubs$: Subject<any> = new Subject()
+  totalPrice = 0
+  constructor(
+    private router: Router,
+    private cartService: CartService,
+    private ordersService: OrdersService
+  ) { }
 
   ngOnInit(): void {
+    this._getOrderSummary()
+  }
+
+  ngOnDestroy(): void {
+    this.endSubs$.next();
+    this.endSubs$.complete();
+  }
+  _getOrderSummary() {
+    this.cartService.cart$.pipe(takeUntil(this.endSubs$)).subscribe((cart) => {
+      this.totalPrice = 0;
+      if (cart) {
+        cart.items?.map((item) => {
+          this.ordersService
+            .getProduct(item.productId)
+            .pipe(take(1))
+            .subscribe((product) => {
+              this.totalPrice += product.price * item.quantity;
+            });
+        });
+      }
+    });
   }
 
 }
