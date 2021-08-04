49a0fdfed904c470a20f831e23ef18dc2834419f
DESKTOP-FBH0P1R\34605 <camposvmiguelangel@gmail.com>
2021-08-04 14:04:50 +0200

    product details add to cart feature

--- libs/products/src/lib/pages/product-details/product-details.component.ts
+++ libs/products/src/lib/pages/product-details/product-details.component.ts
@@ -1,44 +1,48 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem, CartService } from '@mcampos/orders';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Product } from '../../models/product';
 import { ProductsService } from '../../services/products.service';
 
 @Component({
-  selector: 'mcampos-product-details',
-  templateUrl: './product-details.component.html',
-  styles: [
-  ]
+    selector: 'mcampos-product-details',
+    templateUrl: './product-details.component.html',
+    styles: []
 })
 export class ProductDetailsComponent implements OnInit, OnDestroy {
+    product!: Product;
+    endSubs$: Subject<any> = new Subject();
+    quantity = 1;
 
-  
-  product! : Product
-  endSubs$ : Subject<any> = new Subject()
-  quantity= 0
+    constructor(private productsService: ProductsService, private route: ActivatedRoute, private cartService: CartService) {}
 
-  constructor(
-    private productsService : ProductsService,
-    private route : ActivatedRoute
-  ) { }
+    ngOnInit(): void {
+        if (this.route.params) {
+            this.route.params.subscribe((params) => {
+                this._getProduct(params.productId);
+            });
+        }
+    }
+    ngOnDestroy() {
+        this.endSubs$.next();
+        this.endSubs$.complete();
+    }
+    addItemToCart(){
+      const cartItem : CartItem = {
+        productId: this.product.id,
+        quantity: this.quantity
+      }
+      this.cartService.setCartItem(cartItem)
+    }
 
-  ngOnInit(): void {
-    if(this.route.params){
-      this.route.params.subscribe(params => {
-        this._getProduct(params.productId)
-      })
-    }    
-  }
-  ngOnDestroy(){
-    this.endSubs$.next()
-    this.endSubs$.complete()
-  }
-
-private _getProduct(id: string){
-  this.productsService.getProduct(id).pipe(takeUntil(this.endSubs$)).subscribe(product=> {
-    this.product = product
-  })
-}
-
+    private _getProduct(id: string) {
+        this.productsService
+            .getProduct(id)
+            .pipe(takeUntil(this.endSubs$))
+            .subscribe((product) => {
+                this.product = product;
+            });
+    }
 }
