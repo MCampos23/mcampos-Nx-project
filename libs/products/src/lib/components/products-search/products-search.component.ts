a01b4fc4eb258a04c439c6b396c971d3c0174e8f
Miguel Campos <63472933+MCampos23@users.noreply.github.com>
2021-08-21 14:38:59 +0200

    search bar function

--- libs/products/src/lib/components/products-search/products-search.component.ts
+++ libs/products/src/lib/components/products-search/products-search.component.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @angular-eslint/component-selector */
 import { Component } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'products-search',
@@ -7,8 +8,38 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
+
 export class ProductsSearchComponent{
 
-  searchValue = ""
+ 
+  nameAndId = []
+
+  constructor(private productsService: ProductsService) { }
+
+ searchProduct(searchValue : string){
+
+   //const searchValueWords = searchValue.toLocaleUpperCase().split(" ")
+   
+   
+   this.productsService.getProducts().subscribe( products =>{
+   
+    this.nameAndId = products.map(product =>{
+      product.name.toLocaleUpperCase().split(" ")       
+      product.id
+     })     
+  })
+
+  console.log(this.nameAndId)
+ }
  
 }
+
+// productNames.forEach(product =>{
+//   //Recorrer cada palabra de cada array producto 
+
+//   for(let z = 0; z < searchValueWords.length; z++){    
+//     if(product.includes(searchValueWords[z]) && !result.includes(product)){
+//       result.push(product)
+//     }  
+//   }
+//   })
